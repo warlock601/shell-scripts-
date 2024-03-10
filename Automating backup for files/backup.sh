e3b6693d4ab92cbd09a7c5b45f2a4d58e8cf5b36
Vivek Raj <vr32288@gmail.com>
2024-03-10 16:26:21 +0530

    Update backup.sh

--- Automating backup for files/backup.sh
+++ Automating backup for files/backup.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
+backup_directory=("/etc" "/home")                                  # these are directories that we wanna backup 
+destination_directory="/root/backup"                               # where we copy bbackup files 
+sudo mkdir -p $destination_directory
+backup_date=$(date +%b-%d-%y)                                      # date that we get on backup files
+
+echo "Starting backup of ${backup_directory[@]}"                   # [@] will take the elements in list one by one
+
+for i in "${backup_directory[@]}";                                             
+do
+        sudo tar -Pczf /tmp/$i-$backup_date.tar.gz $i              # here we create a tar.gz archive using tar command for the directory $i (means passed cmd line argument)   
+
+        if [ $? -eq 0 ];                                           # if status code is 0, means no error, successfully created archive
+        then
+                echo "$i backup succeeded"
+        else
+                echo "$i backup failed"
+        fi
+
+        sudo cp /tmp/$i-$backup_date.tar.gz $destination_directory              # transfer the archive into destination where file is stored as backup
+
+        if [ $? -eq 0 ];
+        then
+                echo "$i transfer succeded"
+        else
+                echo "$i transfer failed"
+        fi
+
+done
+
+sudo rm /tmp/*.gz                                               # remove files created in temporary location
+echo "backup is done"
 
